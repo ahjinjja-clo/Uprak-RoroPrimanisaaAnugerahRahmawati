3ef862619ce779d2ee47b1d224e44743fdc6c684
N <113294876+ahjinjja-clo@users.noreply.github.com>
2023-05-19 23:26:48 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+// Get the menu elements
+var burgerMenu = document.getElementsByClassName('home-burger-menu');
+var homeNav = document.querySelector('.navigation-links1-nav navigation-links1-root-class-name18');
+
+// Add click event listener to the menu toggle button
+burgerMenu.addEventListener('click', function() {
+  // Toggle the display of the menu
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+});
